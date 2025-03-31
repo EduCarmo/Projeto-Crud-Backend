0d6074e899d1f234591780b331a43a34cacd0313
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-03-30 22:40:29 -0300

    implementando o adicionar produto

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const porta = 4000;
 
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/produtos", rotasProdutos);
 
 app.listen(porta, () => {
